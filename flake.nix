slotThe/nix-templates

commit 113e8e5139e849702cc17e55979a158666c69947

Add flake.nix
So I can actually use these :]

flake.nix
@@ -1,0 +1,25 @@
+{
+  outputs = { self }: {
+
+    templates = {
+      python = {
+        path = ./python;
+        description = "Poetry2nix template";
+      };
+      rust = {
+        path = ./rust;
+        description = "Rust-overlay template for nightly";
+      };
+      haskell = {
+        path = ./haskell;
+        description = "CallCabal2nix template";
+      };
+      ocaml = {
+        path = ./ocaml/
+        description = "Opam-nix template";
+      };
+    };
+
+    defaultTemplate = self.templates.haskell;
+  };
+}

haskell/flake.nix
@@ -8,7 +8,6 @@
       pkgs    = nixpkgs.legacyPackages.${system};
       overlay = final: prev: {
         main = prev.callCabal2nix package ./. { };
-        haskell-aoc-util  = prev.callCabal2nix "haskell-aoc-util" ../../util/haskell-aoc-util { };
       };
       haskellPackages = pkgs.haskellPackages.extend overlay;
     in {

commit 3d36f3056e4ada406f4be3f7b809ace6b86b09ed

flake: Fix ocaml path
Whoops.

Fixes: 113e8e5139e849702cc17e55979a158666c69947

flake.nix
@@ -15,7 +15,7 @@
         description = "CallCabal2nix template";
       };
       ocaml = {
-        path = ./ocaml/
+        path = ./ocaml;
         description = "Opam-nix template";
       };
     };